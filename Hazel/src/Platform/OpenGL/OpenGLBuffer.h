96b6a5dde727846b39712a793dfa5cba090fdf2e
hahastronger <2354359542@qq.com>
2023-05-24 20:26:55 +0800

    p31 buffer layer about stride

--- Hazel/src/Platform/OpenGL/OpenGLBuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLBuffer.h
@@ -2,16 +2,26 @@
 #include"Hazel/Renderer/Buffer.h"
 
 namespace Hazel {
-	class OpenGLVertexBuffer :public VertexBuffer 
+	class OpenGLVertexBuffer : public VertexBuffer 
 	{
 	public:
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+
+		virtual const BufferLayout& GetLayout() const override
+		{
+			return m_Layout;
+		}
+		
+
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 
