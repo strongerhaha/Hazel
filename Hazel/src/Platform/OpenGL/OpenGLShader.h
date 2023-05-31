c391052edd64d70040916b2f2534a886d143be6b
hahastronger <2354359542@qq.com>
2023-05-31 17:53:57 +0800

    p44 shader TO file

--- Hazel/src/Platform/OpenGL/OpenGLShader.h
+++ Hazel/src/Platform/OpenGL/OpenGLShader.h
@@ -1,10 +1,14 @@
 #pragma once
 #include"Hazel/Renderer/Shader.h"
 #include"glm/glm.hpp"
+
+
+typedef unsigned int GLenum;
 namespace Hazel {
 	class OpenGLShader:public Shader
 	{
 	public:
+		OpenGLShader(const std::string& filepath);
 		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
 		virtual ~OpenGLShader();
 
@@ -19,6 +23,9 @@ namespace Hazel {
 		void UploadUniformMat3(const std::string name, const glm::mat3& matrix);
 	private:
 		uint32_t m_RendererID;
-		
+	private:
+		std::string ReadFile(const std::string& filepath);
+		std::unordered_map < GLenum, std::string > PreProcess(const std::string& source);
+		void Compile(const std::unordered_map < GLenum, std::string >& shaderSources);
 	};
 }
